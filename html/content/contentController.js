dd38220669539d248ca0fc79a194716f6d16f7b0
M Pia <pia@coolwoc.co.uk>
2015-08-18 18:57:05 +0300

    create directive for animations

--- html/content/contentController.js
+++ html/content/contentController.js
@@ -3,8 +3,31 @@
 	'use strict';
 
 	angular.module('app.content', [])
-	.controller('Contentcontroller', Contentcontroller);
+	.controller('Contentcontroller', Contentcontroller)
+	.directive('toggleElm', toggleElm);
 
 	function Contentcontroller ( $scope ) {}
+
+	function toggleElm () {
+
+		var directive = {
+			link: link,
+			restrict: 'A'
+		}
+
+		return directive;
+
+		function link(scope, elem, attrs) {
+
+			elem.on("click", function() {
+
+	            $('.wrapper').toggleClass('panelAnimates');
+	            elem.find('span').html($('.closeBtn span').text() == 'Open' ? 'Close' : 'Open');
+
+	        });
+
+		}
+
+	}
 
 })();
--- html/menu/menuController.js
+++ html/menu/menuController.js
@@ -3,27 +3,36 @@
 	'use strict';
 
 	angular.module('app.menu',[])
-	.controller('MenuController', MenuController);
+	.controller('MenuController', MenuController)
+	.directive('panelAnimates', panelAnimates);
 
 	function MenuController( Menulist, Projectlist, Projectlabel, Projectfilter, $stateParams ) {
 
-		var MenuController = this,
-			$finishPanel = $('.overview li a'),
-			$target = $('wrapper');
-
-		$(document).on("click", $finishPanel, function() {
-
-            $target.toggleClass('panelAnimates');
-            $('.closeBtn span')
-            	.html($('.closeBtn span').text() == 'Open' ? 'Close' : 'Open');
-
-        });
+		var MenuController = this;
 
 		MenuController.menuList = Menulist.query();
 		MenuController.projectList = Projectlist.query();
 		MenuController.projectLabel = Projectlabel.query();
 		MenuController.projectFilter = Projectfilter.query();
 
+	};
+
+	function panelAnimates() {
+
+		var directive = {
+			link: link,
+			restrict: 'A'
+		}
+
+		return directive;
+
+		function link  (scope, elem, attrs) {
+
+			elem.on('click', function() {
+				$('.wrapper').toggleClass('panelAnimates');
+	            $('.content .closeBtn').find('span').html($('.content .closeBtn').find('span').text() == 'Open' ? 'Close' : 'Open');
+			});
+		}
 	}
 
 })();
