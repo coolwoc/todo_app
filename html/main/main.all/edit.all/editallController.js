f69ad765854614aab1e2bad9185778de92ddecf8
Pia Gamon <pia.gamon@osf-commerce.com>
2017-11-07 15:31:20 +0200

    removed redundant promise

--- html/main/main.all/edit.all/editallController.js
+++ html/main/main.all/edit.all/editallController.js
@@ -40,13 +40,6 @@
 		edittodoController.idEdit = editId[0];
 		edittodoController.task = Alltasks.get({},{'id': editId[0]});
 
-		console.log(edittodoController.task);
-
-		edittodoController.task.$promise.then(function(data){
-			console.log(data);
-			console.log(data.comment);
-		});
-
 		edittodoController.editTask = function() {
 			
 			edittodoController.newTask = {
